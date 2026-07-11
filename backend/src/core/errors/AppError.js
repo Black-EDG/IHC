8adf383cd3165d3fa69148643d709f0955446781
Black-EDG <yhurycristiamanampaquispe@gmail.com>
2026-07-11 10:38:59 -0500

    Eliminar estructura anterior y dejar solo asistencia-colegio-api

--- backend/src/core/errors/AppError.js
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-  constructor(message, statusCode = 500, code = null) {
-    super(message);
-    this.statusCode = statusCode;
-    this.code = code;
-    this.isOperational = true;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
